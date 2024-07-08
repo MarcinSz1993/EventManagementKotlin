2c2436863db222daf96fca5311349f3f70310d1d
MarcinSz1993 <marcinsz1993@hotmail.com>
2024-07-08 20:19:22 +0200

    Start project

---
+++ src/main/kotlin/com/marcinsz1993/eventmanagementkotlin/EventManagementKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.marcinsz1993.eventmanagementkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventManagementKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<EventManagementKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/marcinsz1993/eventmanagementkotlin/EventManagementKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marcinsz1993.eventmanagementkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventManagementKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
